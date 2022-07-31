1e1e1ee9c829fd252f17ca4b8455b9e2d54fab3b
proxophy <isabelthaas@gmail.com>
2022-07-31 13:39:43 +0200

    Typing for mini haskell implemented

--- Code/ti.hs
+++ Code/ti.hs
@@ -9,9 +9,6 @@ instance Show HType where
   show (HFunc r1 r2) = "(" ++ show r1 ++ "->" ++ show r2 ++ ")"
   show (HA t) = t
 
-t1 :: HType
-t1 = HTup (HA "Bool") (HFunc (HFunc (HA "a") (HA "Int")) (HA "Int"))
-
 newtype Constraints = Constr [(HType, HType)]
 
 instance Show Constraints where
@@ -33,7 +30,7 @@ endForm (Constr cs)
     lhs = concatMap (getHA . fst) cs
     rhs = concatMap (getHA . snd) cs
 
--- check constraint
+-- check if constraint is unifiable
 check :: (HType, HType) -> Bool
 check (HA _, HA _) = True
 check (HA x, t)
@@ -44,13 +41,13 @@ check (t, HA x)
   | otherwise = noRecursive x t
 check (_, _) = True
 
-checkConstraints :: Constraints -> Bool
-checkConstraints (Constr cs) = all check cs
-
 -- for x=f(s0,...,sk), check that x!=si for all i
 noRecursive :: String -> HType -> Bool
 noRecursive s1 (HA s2) = s1 /= s2
 noRecursive s1 t2 = s1 `notElem` getHA t2
+
+checkConstraints :: Constraints -> Bool
+checkConstraints (Constr cs) = all check cs
 
 -- swap elements if necessary
 clean :: (HType, HType) -> (HType, HType)
@@ -62,7 +59,7 @@ clean (t1@(HTup _ _), t2@(HA _)) = (t2, t1)
 clean (t1@(HFunc _ _), t2@(HA _)) = (t2, t1)
 clean c = c
 
--- prepare
+-- prepare: check all constraints, remove trivial
 prepare :: Constraints -> Constraints
 prepare (Constr cs)
   | all check cs = res
@@ -70,9 +67,8 @@ prepare (Constr cs)
   where
     res = Constr $ map clean $ filter (uncurry (/=)) cs
 
--- transform constraint
+-- transform constraint: f(r1,r2)=f(s1, s2) to r1=s1, s2=s2
 transform :: (HType, HType) -> [(HType, HType)]
--- f(r1,r2)=f(s1, s2) to r1=s1, s2=s2
 transform (HTup r1 r2, HTup s1 s2) = [(r1, s1), (r2, s2)]
 transform (HFunc r1 r2, HFunc s1 s2) = [(r1, s1), (r2, s2)]
 transform c@(HA x, _) = [c]
@@ -113,30 +109,160 @@ removeDuplicates (Constr cs) = Constr $ aux cs
       | x `elem` xs = aux xs
       | otherwise = x : aux xs
 
-first :: Constraints -> Constraints
-first = prepare
-
-second :: Constraints -> Constraints
-second = transformConstr . first
-
-third :: String -> Constraints -> Constraints
-third t0 = substitution t0 . second
-
-iteration :: String -> Constraints -> Constraints
-iteration t0 con
+iteration :: Constraints -> Constraints
+iteration con
   | checkConstraints res = res
   | otherwise = error "doesn't unfiy"
   where
-    res = prepare $ substitution t0 $ prepare $ transformConstr $ prepare con
+    res = prepare $ substitution "t0" $ prepare $ transformConstr $ prepare con
 
-unification :: String -> Constraints -> Constraints
-unification t0 con
+unification ::  Constraints -> Constraints
+unification con
   | endForm con = removeDuplicates con
-  | otherwise = unification t0 $ iteration t0 con
+  | otherwise = unification $ iteration con
 
-uct = unification "t0"
 
-itt = iteration "t0"
+data MHask
+  = MVar String
+  | MInt Int
+  | MBool Bool
+  | MLambda String MHask
+  | MApp MHask MHask
+  | MIszero MHask
+  | MAdd MHask MHask
+  | MMult MHask MHask
+  | MIf MHask MHask MHask
+  | MTup MHask MHask
+  | MFst MHask
+  | MSnd MHask
+  deriving (Eq)
+
+instance Show MHask where
+  show (MVar v) = v
+  show (MInt n) = show n
+  show (MBool b) = show b
+  show (MLambda x e) = "(\\" ++ x ++ " -> " ++ show e ++ ")"
+  show (MApp e1 e2) = "(" ++ show e1 ++ " " ++ show e2 ++ ")"
+  show (MIszero e) = "(isZero " ++ show e ++ ")"
+  show (MAdd e1 e2) = "(" ++ show e1 ++ " + " ++ show e2 ++ ")"
+  show (MMult e1 e2) = "(" ++ show e1 ++ " * " ++ show e2 ++ ")"
+  show (MIf e0 e1 e2) = "(if " ++ show e0 ++ " then " ++ show e1 ++ " else " ++ show e2 ++ ")"
+  show (MTup e1 e2) = "(" ++ show e1 ++ "," ++ show e2 ++ ")"
+  show (MFst e) = "(fst " ++ show e ++ ")"
+  show (MSnd e) = "(snd  " ++ show e ++ ")"
+
+data Entails = Ent [(MHask, HType)] MHask HType
+
+instance Show Entails where
+  show (Ent cs e t) = concatMap (\(e, t) -> show e ++ ":" ++ show t ++ " ") cs ++ "|- " ++ show e ++ "::" ++ show t
+
+findAssum :: [(MHask, HType)] -> MHask -> HType
+findAssum [] _ = error "no assumptions yet/no more assumptions"
+findAssum ((m, t) : as) mk
+  | m == mk = t
+  | otherwise = findAssum as mk
+
+go :: Entails -> Constraints -> Int -> ([Entails], Constraints, Int)
+-- Var
+go (Ent gamma e@(MVar x) t) constr@(Constr cs) counter =
+  ([], Constr ((findAssum gamma e, t) : cs), counter)
+-- Abs
+go (Ent gamma (MLambda x e) (HFunc t1 t2)) constr@(Constr cs) counter =
+  ([Ent ((MVar x, t1) : gamma) e t2], constr, counter) -- no new constraint
+go (Ent gamma (MLambda x e) t) constr@(Constr cs) counter =
+  ([Ent ((MVar x, tnext) : gamma) e tnextp1], Constr ((t, HFunc tnext tnextp1) : cs), counter + 2) -- new constraint
+  where
+    tnext = HA ("t" ++ show counter)
+    tnextp1 = HA ("t" ++ show (counter + 1))
+-- App
+go (Ent gamma (MApp e1 e2) t) constr@(Constr cs) counter =
+  ([Ent gamma e1 (HFunc tnext t), Ent gamma e2 tnext], constr, counter + 1)
+  where
+    tnext = HA ("t" ++ show counter)
+-- iszero
+go (Ent gamma (MIszero e) t) constr@(Constr cs) counter =
+  ([Ent gamma e (HA "Int")], Constr ((t, HA "Bool") : cs), counter)
+-- int
+go (Ent gamma (MInt n) t) constr@(Constr cs) counter =
+  ([], Constr ((t, HA "Int") : cs), counter)
+-- true/false
+go (Ent gamma (MBool b) t) constr@(Constr cs) counter =
+  ([], Constr ((t, HA "Bool") : cs), counter)
+-- BinOp
+go (Ent gamma (MAdd e1 e2) t) constr@(Constr cs) counter =
+  ([Ent gamma e1 (HA "Int"), Ent gamma e2 (HA "Int")], Constr ((t, HA "Int") : cs), counter)
+go (Ent gamma (MMult e1 e2) t) constr@(Constr cs) counter =
+  ([Ent gamma e1 (HA "Int"), Ent gamma e2 (HA "Int")], Constr ((t, HA "Int") : cs), counter)
+-- If
+go (Ent gamma (MIf b e1 e2) t) constr@(Constr cs) counter =
+  ([Ent gamma b (HA "Bool"), Ent gamma e1 t, Ent gamma e2 t], constr, counter)
+-- Tuple
+go (Ent gamma (MTup e1 e2) (HTup t1 t2)) constr@(Constr cs) counter =
+  ([Ent gamma e1 t1, Ent gamma e2 t2], constr, counter) -- no new constraint
+go (Ent gamma (MTup e1 e2) t) constr@(Constr cs) counter =
+  ([Ent gamma e1 tnext, Ent gamma e2 tnextp1], Constr ((t, HTup tnext tnextp1) : cs), counter + 2) -- new constraint
+  where
+    tnext = HA ("t" ++ show counter)
+    tnextp1 = HA ("t" ++ show (counter + 1))
+-- fst
+go (Ent gamma (MFst e) t) constr@(Constr cs) counter =
+  ([Ent gamma e (HTup t tnext)], constr, counter + 1)
+  where
+    tnext = HA ("t" ++ show counter)
+-- snd
+go (Ent gamma (MSnd e) t) constr@(Constr cs) counter =
+  ([Ent gamma e (HTup tnext t)], constr, counter + 1)
+  where
+    tnext = HA ("t" ++ show counter)
+
+root ::  MHask -> Entails
+root m = Ent [] m (HA "t0")
+
+iter :: ([Entails], Constraints, Int ) -> ([Entails], Constraints, Int )
+iter r@([], constr, counter) =  r
+iter (ent: es, constr, count) = (newents ++ es, newconstr, newcount)
+  where
+    (newents, newconstr, newcount) = go ent constr count
+
+getConstraints :: MHask -> Constraints
+getConstraints e = aux [root e] (Constr []) 1
+
+aux :: [Entails] -> Constraints -> Int -> Constraints
+aux [] constr _ = constr
+aux (ent : es) constr count = aux (newents ++ es) newconstr newcount
+  where
+    (newents, newconstr, newcount) = go ent constr count
+
+findConstraint :: Constraints -> HType ->  HType
+findConstraint (Constr []) t = t
+findConstraint (Constr ((tk, tv):cs)) t
+  | tk == t = tv
+  | otherwise = findConstraint (Constr cs) t
+
+getType :: MHask -> HType
+getType m = findConstraint sol (HA "t0")
+  where
+    sol = unification (getConstraints m)
+
+-- Examples to test
+
+e1 :: MHask
+e1 = MLambda "x" (MIszero (MVar "x"))
+
+e2 :: MHask
+e2 = MLambda "x" (MTup (MApp (MApp (MVar "x") (MInt 1)) (MBool True)) (MApp (MVar "x") (MInt 0)))
+
+e3 :: MHask
+e3 = MApp (MLambda "x" (MLambda "y" (MApp (MVar "y") (MIszero (MApp (MVar "y") (MVar "x")))))) (MBool True)
+
+e4 :: MHask
+e4 = MLambda "x" (MLambda "y" ( MIf  (MApp (MVar "y") (MVar "x")) (MFst (MVar "x")) (MSnd (MSnd (MVar "x"))) ))
+
+e5 :: MHask
+e5 = MIszero (MFst (MAdd (MInt 3) (MInt 5)))
+
+e6 :: MHask
+e6 = MApp (MLambda "x" (MInt 0)) (MApp (MBool True) (MInt 5))
 
 cont :: Constraints
 cont =
@@ -186,40 +312,94 @@ ce =
       (HA "t2", HA "Int")
     ]
 
-data MHask
-  = MVar String
-  | MInt Int
-  | MBool Bool
-  | MLambda String MHask
-  | MApp MHask MHask
-  | MIszero MHask
-  | MAdd MHask MHask
-  | MMult MHask MHask
-  | MIf MHask MHask MHask
-  | MTup MHask MHask
-  | MFst MHask
-  | MSnd MHask
-  deriving (Eq)
+ent2 :: Entails
+ent2 = Ent [(MVar "x", HA "t1")] (MVar "x") (HA "t2")
 
-instance Show MHask where
-  show (MVar v) = v
-  show (MInt n) = show n
-  show (MBool b) = show b
-  show (MLambda x t) = "(\\" ++ x ++ " -> " ++ show t ++ ")"
-  show (MApp t1 t2) = "(" ++ show t1 ++ " " ++ show t2 ++ ")"
-  show (MIszero t) = "(isZero " ++ show t ++ ")"
-  show (MAdd t1 t2) = "(" ++ show t1 ++ " + " ++ show t2 ++ ")"
-  show (MMult t1 t2) = "(" ++ show t1 ++ " * " ++ show t2 ++ ")"
-  show (MIf t0 t1 t2) = "(if " ++ show t0 ++ " then " ++ show t1 ++ " else " ++ show t2 ++ ")"
-  show (MTup t1 t2) = "(" ++ show t1 ++ "," ++ show t2 ++ ")"
-  show (MFst t) = "(fst " ++ show t ++ ")"
-  show (MSnd t) = "(snd  " ++ show t ++ ")"
+ent3 :: Entails
+ent3 = Ent [] (MApp (MVar "x") (MVar "y")) (HA "t0")
 
-m1 :: MHask
-m1 = MLambda "x" (MTup (MApp (MApp (MVar "x") (MInt 1)) (MBool True)) (MApp (MVar "x") (MInt 0)))
+ent4 :: Entails
+ent4 = Ent [] (MIszero (MVar "x")) (HA "t0")
 
+ent5 :: Entails
+ent5 = Ent [] (MBool True) (HA "t0")
 
-data Rule = RVar | RAbs | RApp | RIszero | RInt | RTrue | RFalse | RBinOp | RIf | RTup | RFst | RSnd 
-data DerTree = Leaf | UNode [(MHask, HType)] (MHask, HType) Rule
+ent6 :: Entails
+ent6 = Ent [] (MAdd (MVar "x") (MVar "y")) (HA "t0")
+
+ent7 :: Entails
+ent7 = Ent [] (MIf (MVar "b") (MVar "x") (MVar "y")) (HA "t0")
+
+ent8 :: Entails
+ent8 = Ent [] (MTup (MVar "x") (MVar "y")) (HTup (HA "t0") (HA "t1"))
+
+ent9 :: Entails
+ent9 = Ent [] (MSnd (MVar "x")) (HA "t0")
+
+-- Code dump
+
+-- applyRule :: Entails -> [Entails]
+-- -- Var
+-- applyRule (Ent gamma (MVar x) _) = []
+-- -- Abs
+-- applyRule (Ent gamma (MLambda x e) (HFunc t1 t2)) = [Ent ((MVar x, t1) : gamma) e t2]
+-- -- App
+-- applyRule (Ent gamma (MApp e1 e2) t1) = [Ent gamma e1 (HFunc (HA "a") t1), Ent gamma e2 (HA "a")]
+-- -- iszero
+-- applyRule (Ent gamma (MIszero e) (HA "Bool")) = [Ent gamma e (HA "Int")]
+-- -- Int
+-- applyRule (Ent gamma (MInt n) (HA "Int")) = []
+-- -- True/False
+-- applyRule (Ent gamma (MBool b) (HA "Bool")) = []
+-- -- BinOp
+-- applyRule (Ent gamma (MAdd e1 e2) (HA "Int")) = [Ent gamma e1 (HA "Int"), Ent gamma e2 (HA "Int")]
+-- applyRule (Ent gamma (MMult e1 e2) (HA "Int")) = [Ent gamma e1 (HA "Int"), Ent gamma e2 (HA "Int")]
+-- -- if
+-- applyRule (Ent gamma (MIf b e1 e2) t) = [Ent gamma b (HA "Bool"), Ent gamma e1 t, Ent gamma e2 t]
+-- -- Tuple
+-- applyRule (Ent gamma (MTup e1 e2) (HTup t1 t2)) = [Ent gamma e1 t1, Ent gamma e2 t2]
+-- -- fst
+-- applyRule (Ent gamma (MFst e) t) = [Ent gamma e (HTup t (HA "b"))]
+-- -- snd
+-- applyRule (Ent gamma (MSnd e) t) = [Ent gamma e (HTup (HA "a") t)]
+-- applyRule ent = error (show ent ++ " type is not correct")
+
+-- -- sometimes verifies if a type is correct -> alpha conversion not implemented
+-- verify :: Entails -> [Entails]
+-- verify ent = aux $ applyRule ent
+--   where
+--     aux [] = []
+--     aux xs = aux $ concatMap applyRule xs
+
+-- data DerTree = Leaf | UNode Rule Entails DerTree | BNode Rule Entails DerTree DerTree | TNode Rule Entails DerTree DerTree DerTree
+
+-- instance Show DerTree where
+--   show Leaf = "_"
+--   show (UNode r ent st) = "(" ++ show ent ++ " [" ++ show r ++ "] " ++ show st ++ ")"
+--   show (BNode r ent st1 st2) = "(" ++ show ent ++ " [" ++ show r ++ "] " ++ show st1 ++ " | " ++ show st2 ++ ")"
+--   show (TNode r ent st1 st2 st3) = "(" ++show ent ++ " [" ++ show r ++ "] " ++ show st1 ++ " | " ++ show st2 ++ " | " ++ show st3 ++ ")"
+
+-- vard :: DerTree
+-- vard = UNode RVar ent Leaf
+
+-- absd :: DerTree
+-- absd = BNode RAbs ent vard vard
 
 -- getType :: MHask -> HType
+
+-- data Rule = RVar | RAbs | RApp | RIszero | RInt | RTrue | RFalse | RPlus | RTimes | RIf | RTup | RFst | RSnd
+
+-- instance Show Rule where
+--   show RVar = "Var"
+--   show RAbs = "Abs"
+--   show RApp = "App"
+--   show RIszero = "iszero"
+--   show RInt = "Int"
+--   show RTrue = "True"
+--   show RFalse = "False"
+--   show RPlus = "+"
+--   show RTimes = "*"
+--   show RIf = "if"
+--   show RTup = "Tuple"
+--   show RFst = "fst"
+--   show RSnd = "snd"
